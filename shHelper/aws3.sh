ba71f73a3a82b48980a2b05800b989c7f03524ab
kevin zhuang <kevin7441@gmail.com>
2020-02-24 20:17:51 +1100

    added aws3 helpers

---
+++ shHelper/aws3.sh
@@ -0,0 +1,36 @@
+# aws3 helper functions
+
+function get_local_file() {
+  if [[ -z "$path_to_file" ]]
+  then
+    if [[ ! -z "$search_from_root" ]]
+    then
+      cd "$HOME"
+    fi
+    # popup fzf to select a file or dir based on flags
+    if [[ "$1" != 'folder' ]]
+    then
+      echo "select a local file to upload"
+      if [[ -z "$hidden" ]]
+      then
+        path_to_file=$(fd --type f | fzf)
+      else
+        path_to_file=$(fd --type f -H | fzf)
+      fi
+    else
+      echo "select a local dir to upload recursively"
+      if [[ -z "$hidden" ]]
+      then
+        path_to_file=$(fd --type d | fzf) 
+      else
+        path_to_file=$(fd --type d -H | fzf)
+      fi
+    fi
+    # exit if no file selected
+    [[ -z "$path_to_file" ]] && exit 1
+    if [[ ! -z "$search_from_root" ]]
+    then
+      path_to_file="$HOME/$path_to_file"
+    fi
+  fi
+}
