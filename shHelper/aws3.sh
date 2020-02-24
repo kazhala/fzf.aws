c7e9f2c3724d3d6a82875bbc4905d8cb937446ee
kevin zhuang <kevin7441@gmail.com>
2020-02-24 20:53:03 +1100

    check if fd exists

--- shHelper/aws3.sh
+++ shHelper/aws3.sh
@@ -1,15 +1,48 @@
 # aws3 helper functions
 
+function fd_exists() {
+  fd -V &>/dev/null
+  echo "$?"
+}
+
+# get local file path
+# @params
+# $1: 'folder', search for folder instead of file
 function get_local_file() {
+  # if not specifed through the -P path flag
+  # search local files
   if [[ -z "$path_to_file" ]]
   then
+    # if root flag specifed, go to root and search
     if [[ ! -z "$search_from_root" ]]
     then
       cd "$HOME"
     fi
     # popup fzf to select a file or dir based on flags
+    # search for folders if parameter specifed
     if [[ "$1" != 'folder' ]]
     then
+      search_file
+    else
+      search_file 'folder'
+    fi
+    # exit if no file selected
+    [[ -z "$path_to_file" ]] && exit 1
+    if [[ ! -z "$search_from_root" ]]
+    then
+      path_to_file="$HOME/$path_to_file"
+    fi
+  fi
+}
+
+# execute fzf to search the file
+function search_file() {
+  exe_fd=$(fd_exists)
+  if [[ "$exe_fd" -eq 0 ]]
+  then
+    if [[ "$1" != 'folder' ]]
+    then
+      # if hidden flag, search for hidden file
       if [[ -z "$hidden" ]]
       then
         path_to_file=$(fd --type f | fzf)
@@ -17,6 +50,7 @@ function get_local_file() {
         path_to_file=$(fd --type f -H | fzf)
       fi
     else
+      # if hidden flag, search for hidden dir
       if [[ -z "$hidden" ]]
       then
         path_to_file=$(fd --type d | fzf) 
@@ -24,11 +58,12 @@ function get_local_file() {
         path_to_file=$(fd --type d -H | fzf)
       fi
     fi
-    # exit if no file selected
-    [[ -z "$path_to_file" ]] && exit 1
-    if [[ ! -z "$search_from_root" ]]
+  else
+    if [[ "$1" != 'folder' ]]
     then
-      path_to_file="$HOME/$path_to_file"
+      path_to_file=$(find * -type f | fzf)
+    else
+      path_to_file=$(find * -type d | fzf)
     fi
   fi
 }
