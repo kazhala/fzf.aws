aef587eadbf3bc6086a057345422531c8967f05b
kevin zhuang <kevin7441@gmail.com>
2020-03-01 16:39:35 +1100

    refactor help for awsec2

---
+++ faws_sh/awsec2/usage.sh
@@ -0,0 +1,28 @@
+# help message for awsec2
+# @params
+# $1: action_type
+
+function usage() {
+  local action="$1"
+  if [[ -z "$action" ]]; then
+    echo "usage: awsec2 [-h] {ssh,stop,terminate} ...\n"
+    echo "perform actions on the selected instance\n"
+    echo "positional arguments:"
+    echo "  {ssh,stop,terminate}\n"
+    echo "optional arguments:"
+    echo "  -h\t\tshow this help message and exit"
+  elif [[ "$action" == 'ssh' ]]; then
+    echo "usage: awsec2 ssh [-h] [-r] [-p]\n"
+    echo "ssh into the selected instance\n"
+    echo "optional arguments:"
+    echo "  -h\t\tshow this help message and exit"
+    echo "  -r\t\tselect a different region rather than using the default region"
+    echo "  -p PATH\tspecify a different path than config for the location of the key pem file"
+  else
+    echo "usage: awsec2 $action [-h] [-r]\n"
+    echo "$action the selected instance\n"
+    echo "optional arguments:"
+    echo "  -h\t\tshow this help message and exit"
+    echo "  -r\t\tselect a different region rather than using the default region"
+  fi
+}
