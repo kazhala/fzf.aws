8438ff7acb096e82398614fcd10eb1f3bc4ad5ac
kevin zhuang <kevin7441@gmail.com>
2020-03-02 21:08:41 +1100

    update helper message

--- src/shsrc/ec2/usage.sh
+++ src/shsrc/ec2/usage.sh
@@ -1,25 +1,25 @@
-# help message for awsec2
+# help message for ec2
 # @params
 # $1: action_type
 
 function usage() {
   local action="$1"
   if [[ -z "$action" ]]; then
-    echo "usage: awsec2 [-h] {ssh,stop,terminate} ...\n"
+    echo "usage: faws ec2 [-h] {ssh,stop,terminate} ...\n"
     echo "perform actions on the selected instance\n"
     echo "positional arguments:"
     echo "  {ssh,stop,terminate}\n"
     echo "optional arguments:"
     echo "  -h\t\tshow this help message and exit"
   elif [[ "$action" == 'ssh' ]]; then
-    echo "usage: awsec2 ssh [-h] [-r] [-p]\n"
+    echo "usage: faws ec2 ssh [-h] [-r] [-p]\n"
     echo "ssh into the selected instance\n"
     echo "optional arguments:"
     echo "  -h\t\tshow this help message and exit"
     echo "  -r\t\tselect a different region rather than using the default region"
     echo "  -p PATH\tspecify a different path than config for the location of the key pem file"
   else
-    echo "usage: awsec2 $action [-h] [-r]\n"
+    echo "usage: faws ec2 $action [-h] [-r]\n"
     echo "$action the selected instance\n"
     echo "optional arguments:"
     echo "  -h\t\tshow this help message and exit"
--- src/shsrc/s3/usage.sh
+++ src/shsrc/s3/usage.sh
@@ -1,4 +1,4 @@
-# help message to display for aws3
+# help message to display for faws s3
 # @params
 # $1: operation type
 
@@ -6,7 +6,7 @@
 function usage() {
   local operation_type="$1"
   if [[ -z "$operation_type" ]]; then
-    echo "usage: aws3 [-h] {upload,download,delete,bucket,presign} ...\n"
+    echo "usage: faws s3 [-h] {upload,download,delete,bucket,presign} ...\n"
     echo "perform CRUD operation with cp/mv/rm in s3 bucket interactively"
     echo "without positional arguments, it will only print the selected item s3 path\n"
     echo "positional arguments:"
@@ -14,7 +14,7 @@ function usage() {
     echo "optional arguments:"
     echo "  -h\t\tshow this help message and exit"
   elif [[ "$operation_type" == 'bucket' ]]; then
-    echo "usage: aws3 bucket [-h] [-p] [-m] [-s] [-r]\n"
+    echo "usage: faws s3 bucket [-h] [-p] [-m] [-s] [-r]\n"
     echo "transfer file between buckets\n"
     echo "optional arguments:"
     echo "  -h\t\tshow this help message and exit"
@@ -23,21 +23,21 @@ function usage() {
     echo "  -s\t\tuse sync instead of cp command, recursively copies new and updated files from one bucket to another"
     echo "  -r\t\toperate recursively, set this flag when manipulating folders"
   elif [[ "$operation_type" == 'delete' ]]; then
-    echo "usage: aws3 delete [-h] [-p] [-r]\n"
+    echo "usage: faws s3 delete [-h] [-p] [-r]\n"
     echo "delete files on a s3 bucket, to delete folder, set -r flag\n"
     echo "optional arguments:"
     echo "  -h\t\tshow this help message and exit"
     echo "  -p PATH\t\tspecify a s3 path (bucketName/path) after this flag and skip s3 bucket/path selection"
     echo "  -r\t\toperate recursively, set this flag when manipulating folders"
   elif [[ "$operation_type" == 'presign' ]]; then
-    echo "usage: aws3 presign [-h] [-p] [-t]\n"
+    echo "usage: faws s3 presign [-h] [-p] [-t]\n"
     echo "generate a temprary download url for the selected s3 object"
     echo "optional arguments:\n"
     echo "  -h\t\tshow this help message and exit"
     echo "  -p PATH\tspecify a s3 path (bucketName/path) after this flag and skip s3 bucket/path selection"
     echo "  -t TIME\tspecify the expire time of the url in seconds, default is 3600s"
   elif [[ "$operation_type" == 'upload' || "$operation_type" == 'download' ]]; then
-    echo "usage: aws3 "$operation_type" [-h] [-p] [-P] [-m] [-s] [-r] [-R] [-H]\n"
+    echo "usage: faws s3 "$operation_type" [-h] [-p] [-P] [-m] [-s] [-r] [-R] [-H]\n"
     echo "$operation_type from/to a selected bucket\n"
     echo "optional arguments:\n"
     echo "  -h\t\tshow this help message and exit"
