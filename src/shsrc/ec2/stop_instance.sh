ada1bc808a4e9b85286366ac9f866bff940234cd
kevin zhuang <kevin7441@gmail.com>
2020-03-06 17:13:31 +1100

    wait for instance stop

--- src/shsrc/ec2/stop_instance.sh
+++ src/shsrc/ec2/stop_instance.sh
@@ -1,11 +1,14 @@
 # stop the selected instance
 # @params
 # $1: instance_id
-# $instance_status: instance_status
+# $2: instance_status
+# $3: wait_flag
+
 
 function stop_instance() {
   local instance_id="$1"
   local instance_status="$2"
+  local wait_flag="$3"
   if [[ "$instance_status" == stopped ]]; then
     echo "Instance is already stopped"
   # stpp the instance if it is running
@@ -13,6 +16,11 @@ function stop_instance() {
     get_confirmation "Instance will be stopped, continue?"
     if [[ $confirm == 'y' ]]; then
       aws ec2 stop-instances --instance-ids "$instance_id" --output text
+      if [[ "$wait_flag" == 'true' ]]; then
+        echo "Waiting for instance to be stopped"
+        aws ec2 wait instance-stopped --instance-ids "$instance_id"
+        echo "Instance stopped"
+      fi
     fi
   else
     # exit if instance state is not running or stopped 
