50e1f2f8a07f4b7944bd3f006c8b20413c69541a
kevin zhuang <kevin7441@gmail.com>
2020-03-06 17:16:40 +1100

    wait instance terminated

--- src/shsrc/ec2/terminate_instance.sh
+++ src/shsrc/ec2/terminate_instance.sh
@@ -1,11 +1,18 @@
 # terminate the selected instance
 # @params
 # $1: instance_id
+# $2: wait_flag
 
 function terminate_instance() {
   local instance_id="$1"
+  local wait_flag="$2"
   get_confirmation "Instance will be terminated, continue?"
   if [[ "$confirm" == 'y' ]]; then
     aws ec2 terminate-instances --instance-ids "$instance_id" --output text
+    if [[ "$wait_flag" == 'true' ]]; then
+      echo "Waiting for instance to be terminated"
+      aws ec2 wait instance-terminated --instance-ids "$instance_id"
+      echo "Instance terminated"
+    fi
   fi
 }
