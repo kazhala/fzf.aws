124d8af7a3b446e56fa029a3cf71d5ad8e6df8c6
kevin zhuang <kevin7441@gmail.com>
2020-03-06 20:14:40 +1100

    update no argument menu

--- src/shsrc/ec2/start_instance.sh
+++ src/shsrc/ec2/start_instance.sh
@@ -6,14 +6,14 @@
 function start_instance() {
   local instance_id="$1"
   local wait_flag="$2"
-    get_confirmation "Instance will be started, continue?"
-    if [[ "$confirm" == 'y' ]]; then
-      echo "Starting instance now.."
-      aws ec2 start-instances --instance-ids $instance_id --output text
-      if [[ -n "$wait_flag" ]]; then
-        echo "Waiting for instance to be ready.."
-        aws ec2 wait instance-running --instance-ids $instance_id
-        echo "Instance is ready, run faws ec2 ssh to connect"
-      fi
+  get_confirmation "Instance will be started, continue?"
+  if [[ "$confirm" == 'y' ]]; then
+    echo "Starting instance now.."
+    aws ec2 start-instances --instance-ids $instance_id --output text
+    if [[ -n "$wait_flag" ]]; then
+      echo "Waiting for instance to be ready.."
+      aws ec2 wait instance-running --instance-ids $instance_id
+      echo "Instance is ready, run faws ec2 ssh to connect"
     fi
+  fi
 }
--- src/shsrc/ec2/stop_instance.sh
+++ src/shsrc/ec2/stop_instance.sh
@@ -7,7 +7,7 @@ function stop_instance() {
   local instance_id="$1"
   local wait_flag="$2"
   get_confirmation "Instance will be stopped, continue?"
-  if [[ $confirm == 'y' ]]; then
+  if [[ "$confirm" == 'y' ]]; then
     aws ec2 stop-instances --instance-ids $instance_id --output text
     if [[ -n "$wait_flag" ]]; then
       # wait for instance to be stopped
