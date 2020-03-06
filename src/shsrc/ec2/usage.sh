375f0871ce1f2b4c1fe8fa37ad4ccfd5fae1cd07
kevin zhuang <kevin7441@gmail.com>
2020-03-06 18:19:38 +1100

    update ec2 usage

--- src/shsrc/ec2/usage.sh
+++ src/shsrc/ec2/usage.sh
@@ -1,37 +1,36 @@
 # help message for ec2
 # @params
-# $1: action_type
+# $1: action_command
 
 function usage() {
-  local action="$1"
-  if [[ -z "$action" ]]; then
+  local action_command="$1"
+  if [[ -z "$action_command" ]]; then
     echo "usage: faws ec2 [-h] {ssh,stop,terminate} ...\n"
     echo "perform actions on the selected instance\n"
     echo "positional arguments:"
     echo "  {ssh,stop,terminate}\n"
     echo "optional arguments:"
     echo "  -h\t\tshow this help message and exit"
-  elif [[ "$action" == 'ssh' ]]; then
-    echo "usage: faws ec2 ssh [-h] [-r] [-p] [-w] [-u]\n"
+  elif [[ "$action_command" == 'ssh' ]]; then
+    echo "usage: faws ec2 ssh [-h] [-r] [-p] [-u]\n"
     echo "ssh into the selected instance\n"
     echo "optional arguments:"
     echo "  -h\t\tshow this help message and exit"
     echo "  -r\t\tselect a different region rather than using the default region"
     echo "  -p PATH\tspecify a different path than config for the location of the key pem file"
-    echo "  -w\t\tpause the program and wait for instance to be running if it is currently stopped"
     echo "  -u NAME\tspecify a different username used to ssh into the instance, default is ec2-user"
-  elif [[ "$action" == 'reboot' ]]; then
-    echo "usage: faws ec2 reboot [-h] [-r] [-w]\n"
+  elif [[ "$action_command" == 'reboot' ]]; then
+    echo "usage: faws ec2 reboot [-h] [-r]\n"
     echo "reboot the selected instance\n"
     echo "optional arguments:"
     echo "  -h\t\tshow this help message and exit"
     echo "  -r\t\tselect a different region rather than using the default region"
   else
-    echo "usage: faws ec2 $action [-h] [-r] [-w]\n"
-    echo "$action the selected instance\n"
+    echo "usage: faws ec2 $action_command [-h] [-r] [-w]\n"
+    echo "$action_command the selected instance\n"
     echo "optional arguments:"
     echo "  -h\t\tshow this help message and exit"
     echo "  -r\t\tselect a different region rather than using the default region"
-    echo "  -w\t\tpause the program and wait for instance $action complete"
+    echo "  -w\t\tpause the program and wait for instance $action_command complete"
   fi
 }
