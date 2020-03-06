d41892ae3206c66ed2ac3d820a45e498b63b41be
kevin zhuang <kevin7441@gmail.com>
2020-03-06 17:39:48 +1100

    update message display for reboot

--- src/shsrc/ec2/reboot_instance.sh
+++ src/shsrc/ec2/reboot_instance.sh
@@ -9,5 +9,6 @@ function reboot_instance() {
   if [[ "$confirm" == 'y' ]]; then
     aws ec2 reboot-instances --instance-ids "$instance_id" --output text
     echo "Instance is being placed in the reboot queue"
+    echo "It may take aws up to 4mins before it is rebooted, instance will remain in running state"
   fi
 }
