b2b24c18cae8a6c1deceef50517d1b63343af15e
kevin zhuang <kevin7441@gmail.com>
2020-04-24 11:38:00 +1000

    init cloudwatch

---
+++ fzfaws/cloudwatch/cloudwatch.py
@@ -0,0 +1,45 @@
+"""contains wrapper class for cloudwatch
+
+wrapps around boto3 client for centerilised management
+"""
+from fzfaws.utils.session import BaseSession
+from fzfaws.utils.pyfzf import Pyfzf
+
+
+class Cloudwatch(BaseSession):
+    """cloudwatch wrapper class
+
+    Handles all operation related to cloudwatch
+
+    Attributes:
+        client: boto3 client
+        resource: boto3 resource
+        region: region for the operation
+        profile: profile used for the operation
+        arns: selected arns
+    """
+
+    def __init__(self, profile=None, region=None):
+        super().__init__(profile=profile, region=region, service_name='cloudwatch')
+        self.arns = []
+
+    def set_arns(self, arns=[], empty_allow=False, header=None, multi_select=False):
+        """set cloudwatch arns for operation
+
+        Args:
+            arns: list, list of arns and skip fzf
+            empty_allow: bool, if allow empty selection
+            header: string, helper message to display in fzf header
+            multi_select: bool, if multi_select is enabled
+        """
+        if not arns:
+            fzf = Pyfzf()
+            paginator = self.client.get_paginator('describe_alarms')
+            for result in paginator.paginate():
+                if result.get('CompositeAlarms'):
+                    fzf.process_list(result['CompositeAlarms'], 'AlarmArn')
+                if result.get('MetricAlarms'):
+                    fzf.process_list(result['MetricAlarms'], 'AlarmArn')
+            arns = fzf.execute_fzf(
+                empty_allow=empty_allow, multi_select=True, header=header)
+        self.arns = arns
