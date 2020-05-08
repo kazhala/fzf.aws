3e3f4f043774fe9d00af052102d4d0e3421d2bf4
kevin zhuang <kevin7441@gmail.com>
2020-05-08 16:08:53 +1000

    fix ls_s3

--- fzfaws/s3/ls_s3.py
+++ fzfaws/s3/ls_s3.py
@@ -28,9 +28,9 @@ def ls_s3(profile=False, bucket=False, version=False, deletemark=False):
     if deletemark:
         version = True
     if not bucket:
-        s3.set_s3_object(multi_select=True, version=version,
-                         deletemark=deletemark)
+        s3.set_s3_object(multi_select=True, version=version, deletemark=deletemark)
 
+    obj_versions = []  # type: list
     if version:
         obj_versions = s3.get_object_version()
 
@@ -39,31 +39,30 @@ def ls_s3(profile=False, bucket=False, version=False, deletemark=False):
         acls = s3.client.get_bucket_acl(Bucket=s3.bucket_name)
         versions = s3.client.get_bucket_versioning(Bucket=s3.bucket_name)
         region = s3.client.get_bucket_location(Bucket=s3.bucket_name)
-        response['Owner'] = acls.get('Owner')
-        response['Region'] = region.get('LocationConstraint')
+        response["Owner"] = acls.get("Owner")
+        response["Region"] = region.get("LocationConstraint")
         try:
             encryption = s3.client.get_bucket_encryption(Bucket=s3.bucket_name)
-            response['Encryption'] = encryption.get(
-                'ServerSideEncryptionConfiguration')
+            response["Encryption"] = encryption.get("ServerSideEncryptionConfiguration")
         except:
-            response['Encryption'] = None
+            response["Encryption"] = None
         try:
             public = s3.client.get_bucket_policy_status(Bucket=s3.bucket_name)
-            response['Public'] = public.get('PolicyStatus').get('IsPublic')
+            response["Public"] = public.get("PolicyStatus").get("IsPublic")
             policy = s3.client.get_bucket_policy(Bucket=s3.bucket_name)
-            response['Policy'] = policy.get('Policy')
+            response["Policy"] = policy.get("Policy")
         except:
             pass
-        response['Grants'] = acls.get('Grants')
-        response['Versioning'] = versions.get('Status')
-        response['MFA'] = versions.get('MFADelete')
+        response["Grants"] = acls.get("Grants")
+        response["Versioning"] = versions.get("Status")
+        response["MFA"] = versions.get("MFADelete")
         try:
             tags = s3.client.get_bucket_tagging(Bucket=s3.bucket_name)
-            response['Tags'] = tags.get('TagSet')
+            response["Tags"] = tags.get("TagSet")
         except:
-            response['Tags'] = None
-        print(80*'-')
-        print('s3://%s' % s3.bucket_name)
+            response["Tags"] = None
+        print(80 * "-")
+        print("s3://%s" % s3.bucket_name)
         print(json.dumps(response, indent=4, default=str))
         exit()
 
@@ -71,46 +70,39 @@ def ls_s3(profile=False, bucket=False, version=False, deletemark=False):
         for obj_version in obj_versions:
             response = s3.client.head_object(
                 Bucket=s3.bucket_name,
-                Key=obj_version.get('Key'),
-                VersionId=obj_version.get('VersionId')
+                Key=obj_version.get("Key"),
+                VersionId=obj_version.get("VersionId"),
             )
             tags = s3.client.get_object_tagging(
                 Bucket=s3.bucket_name,
-                Key=obj_version.get('Key'),
-                VersionId=obj_version.get('VersionId')
+                Key=obj_version.get("Key"),
+                VersionId=obj_version.get("VersionId"),
             )
             acls = s3.client.get_object_acl(
                 Bucket=s3.bucket_name,
-                Key=obj_version.get('Key'),
-                VersionId=obj_version.get('VersionId')
+                Key=obj_version.get("Key"),
+                VersionId=obj_version.get("VersionId"),
             )
-            response.pop('ResponseMetadata', None)
-            response['Tags'] = tags.get('TagSet')
-            response['Owner'] = acls.get('Owner')
-            response['Grants'] = acls.get('Grants')
-            print(80*'-')
-            print('s3://%s/%s versioned %s' % (s3.bucket_name,
-                                               obj_version.get('Key'), obj_version.get('VersionId')))
+            response.pop("ResponseMetadata", None)
+            response["Tags"] = tags.get("TagSet")
+            response["Owner"] = acls.get("Owner")
+            response["Grants"] = acls.get("Grants")
+            print(80 * "-")
+            print(
+                "s3://%s/%s versioned %s"
+                % (s3.bucket_name, obj_version.get("Key"), obj_version.get("VersionId"))
+            )
             print(json.dumps(response, indent=4, default=str))
 
     else:
         for s3_key in s3.path_list:
-            response = s3.client.head_object(
-                Bucket=s3.bucket_name,
-                Key=s3_key,
-            )
-            tags = s3.client.get_object_tagging(
-                Bucket=s3.bucket_name,
-                Key=s3_key
-            )
-            acls = s3.client.get_object_acl(
-                Bucket=s3.bucket_name,
-                Key=s3_key
-            )
-            response.pop('ResponseMetadata', None)
-            response['Tags'] = tags.get('TagSet')
-            response['Owner'] = acls.get('Owner')
-            response['Grants'] = acls.get('Grants')
-            print(80*'-')
-            print('s3://%s/%s' % (s3.bucket_name, s3_key))
+            response = s3.client.head_object(Bucket=s3.bucket_name, Key=s3_key,)
+            tags = s3.client.get_object_tagging(Bucket=s3.bucket_name, Key=s3_key)
+            acls = s3.client.get_object_acl(Bucket=s3.bucket_name, Key=s3_key)
+            response.pop("ResponseMetadata", None)
+            response["Tags"] = tags.get("TagSet")
+            response["Owner"] = acls.get("Owner")
+            response["Grants"] = acls.get("Grants")
+            print(80 * "-")
+            print("s3://%s/%s" % (s3.bucket_name, s3_key))
             print(json.dumps(response, indent=4, default=str))
