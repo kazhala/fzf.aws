7b1b4f7c711668135a4493aaf051a1ed965d6ec9
kevin zhuang <kevin7441@gmail.com>
2020-05-10 15:51:00 +1000

    refactor session

--- fzfaws/utils/session.py
+++ fzfaws/utils/session.py
@@ -4,41 +4,38 @@ Wraps around boto3 session for better profile region management
 """
 from boto3.session import Session
 from fzfaws.utils.pyfzf import Pyfzf
+from typing import Union, Optional
 
 
 class BaseSession:
     """base session class for managing profile and regions
 
-    wraps around boto3 session and should be used for inheritence
-
-    Attributes:
-        session: boto3 session with customized profile and regions
-        client: boto3 client init from the session
-        resource: boto3 resource init from the session
-        region: selected region for operation
-        profile: selected profile for operation
+    :param profile: profile to use for next operation
+    :type profile: Union[str, bool]
+    :param region: region to use for next operation
+    :type region: Union[str, bool]
+    :param service_name: name of boto3 service to init
+    :type service_name: str
     """
 
-    def __init__(self, profile=None, region=None, service_name=None):
+    def __init__(
+        self,
+        profile: Optional[Union[str, bool]] = None,
+        region: Optional[Union[str, bool]] = None,
+        service_name: Optional[str] = None,
+    ) -> None:
         """construtor of BaseSession class
-
-        Args:
-            profile: string or bool, if string, skip fzf and set profile
-                if bool, set profile through fzf, if None, skip use default profile
-            regions: string or bool, if string, skip fzf and set region
-                if bool, set region through fzf, if None, skip use default region
-            service_name: the service name for boto to init
         """
         session = Session()
-        selected_profile = None
-        selected_region = None
+        selected_profile: Optional[str] = None
+        selected_region: Optional[str] = None
         if profile and type(profile) == bool:
             fzf = Pyfzf()
             for profile in session.available_profiles:
                 fzf.append_fzf("%s\n" % profile)
             selected_profile = str(fzf.execute_fzf(print_col=1))
         elif profile and type(profile) == str:
-            selected_profile = profile
+            selected_profile = str(profile)
 
         if region and type(region) == bool:
             fzf = Pyfzf()
@@ -47,9 +44,9 @@ class BaseSession:
                 fzf.append_fzf("%s\n" % region)
             selected_region = str(fzf.execute_fzf(print_col=1))
         elif region and type(region) == str:
-            selected_region = region
-        self.profile = selected_profile
-        self.region = selected_region
+            selected_region = str(region)
+        self.profile: Optional[str] = selected_profile
+        self.region: Optional[str] = selected_region
         self.session = Session(
             region_name=selected_region, profile_name=selected_profile
         )
@@ -60,7 +57,15 @@ class BaseSession:
         if service_name in resources:
             self.resource = self.session.resource(service_name)
 
-    def get_paginated_result(self, paginator_name: str, **kwargs):
+    def get_paginated_result(self, paginator_name: str, **kwargs) -> list:
+        """helper function to retrieve all pginated result
+
+        The purpose of this function rather than inline is for
+        better unit testing
+
+        :param paginator_name: paginator name, check boto3 doc
+        :type paginator_name: str
+        """
         response = []
         for result in self.client.get_paginator(paginator_name).paginate(**kwargs):
             response.append(result)
