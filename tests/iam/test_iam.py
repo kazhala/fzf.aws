f55cb72233aa00c0606fe14de7d62b1ccc984198
kevin zhuang <kevin7441@gmail.com>
2020-05-16 13:36:49 +1000

    refactor iam unittest

--- tests/iam/test_iam.py
+++ tests/iam/test_iam.py
@@ -25,24 +25,12 @@ class TestIAM(unittest.TestCase):
         self.assertEqual(None, iam.region)
 
     @patch.object(BaseSession, "get_paginated_result")
-    @patch.object(Pyfzf, "append_fzf")
-    @patch.object(Pyfzf, "execute_fzf")
-    def test_no_result(self, mocked_fzf_execute, mocked_fzf_append, mocked_result):
-        mocked_result.return_value = []
-        mocked_fzf_execute.return_value = ""
-        self.iam.set_arns(service="cloudformation.amazonaws.com")
-        mocked_fzf_append.assert_not_called()
-        mocked_fzf_execute.assert_called_once()
-        self.assertEqual("", self.iam.arns[0])
-
-    @patch.object(BaseSession, "get_paginated_result")
     @patch.object(Pyfzf, "process_list")
     @patch.object(Pyfzf, "append_fzf")
     @patch.object(Pyfzf, "execute_fzf")
     def test_setarns(
         self, mocked_fzf_execute, mocked_fzf_append, mocked_fzf_list, mocked_result
     ):
-        # test service IAM
         mocked_result.return_value = [
             {
                 "Roles": [
@@ -71,19 +59,16 @@ class TestIAM(unittest.TestCase):
                 ]
             }
         ]
+
+        # general test
         mocked_fzf_execute.return_value = (
             "arn:aws:iam::378756445655:role/admincloudformaitontest"
         )
-        self.iam.set_arns(service="cloudformation.amazonaws.com")
-        mocked_fzf_append.assert_called_with(
-            "RoleName: admincloudformaitontest  Arn: arn:aws:iam::378756445655:role/admincloudformaitontest"
-        )
-        self.assertEqual(
-            "arn:aws:iam::378756445655:role/admincloudformaitontest", self.iam.arns[0]
-        )
-
-        # no restriction test
         self.iam.set_arns()
+        self.assertEqual(
+            self.iam.arns, ["arn:aws:iam::378756445655:role/admincloudformaitontest"]
+        )
+        mocked_fzf_append.assert_not_called()
         mocked_fzf_list.assert_called_with(
             [
                 {
@@ -112,3 +97,37 @@ class TestIAM(unittest.TestCase):
             "RoleName",
             "Arn",
         )
+
+        # parameter test
+        self.iam.set_arns(service="cloudformation.amazonaws.com")
+        mocked_fzf_append.assert_called_with(
+            "RoleName: admincloudformaitontest  Arn: arn:aws:iam::378756445655:role/admincloudformaitontest"
+        )
+
+        mocked_fzf_append.reset_mock()
+        self.iam.set_arns(service="hello")
+        mocked_fzf_append.assert_not_called()
+
+        mocked_fzf_list.reset_mock()
+        self.iam.set_arns(header="hello", empty_allow=True, multi_select=True)
+        mocked_fzf_list.assert_called_once()
+        mocked_fzf_execute.assert_called_with(
+            empty_allow=True, header="hello", multi_select=True, print_col=4
+        )
+
+        self.iam.set_arns(arns="111111")
+        self.assertEqual(self.iam.arns, ["111111"])
+
+        self.iam.set_arns(arns=["111111", "222222"])
+        self.assertEqual(self.iam.arns, ["111111", "222222"])
+
+        # empty result test
+        self.iam.arns = [""]
+        mocked_fzf_execute.reset_mock()
+        mocked_fzf_append.reset_mock()
+        mocked_result.return_value = []
+        mocked_fzf_execute.return_value = ""
+        self.iam.set_arns(service="cloudformation.amazonaws.com")
+        mocked_fzf_append.assert_not_called()
+        mocked_fzf_execute.assert_called_once()
+        self.assertEqual([""], self.iam.arns)
