36ddf4c05320f068fb595badb35e7eee38f89ba5
kevin zhuang <kevin7441@gmail.com>
2020-05-21 09:20:56 +1000

    fileloader unittest global waiter config

--- tests/utils/test_fileloader.py
+++ tests/utils/test_fileloader.py
@@ -1,6 +1,7 @@
 import os
 import json
 import unittest
+from unittest.mock import patch
 from fzfaws.utils import FileLoader
 
 
@@ -42,28 +43,49 @@ class TestFileLoader(unittest.TestCase):
         if "foo" not in result["dictBody"]:
             self.fail("Json file is not read properly")
 
-    def test_load_config_file(self):
+    @patch.object(FileLoader, "_set_gloable_env")
+    @patch.object(FileLoader, "_set_fzf_env")
+    def test_load_config_file(self, mocked_set_fzf, mocked_set_global):
         self.fileloader.path = self.test_yaml
+        self.fileloader.load_config_file(config_path=self.test_yaml)
+        mocked_set_fzf.assert_called_once()
+        mocked_set_global.assert_called_once()
+
+    def test_set_global_env(self):
+        # normal test
+        self.fileloader.load_config_file(config_path=self.test_yaml)
+        self.assertEqual(
+            os.environ["FZFAWS_GLOBAL_WAITER"],
+            json.dumps({"delay": 15, "max_attempts": 40}),
+        )
+
+        # reset
+        os.environ["FZFAWS_GLOBAL_WAITER"] = ""
+
+        # empty test
+        self.fileloader._set_gloable_env({})
+        self.assertEqual(os.environ["FZFAWS_GLOBAL_WAITER"], "")
+
+        # custom settings
+        self.fileloader._set_gloable_env({"waiter": {"delay": 10}})
+        self.assertEqual(os.environ["FZFAWS_GLOBAL_WAITER"], json.dumps({"delay": 10}))
+
+    def test_set_fzf_env(self):
+        # normal test
         self.fileloader.load_config_file(config_path=self.test_yaml)
         self.assertEqual(os.environ["FZFAWS_FZF_EXECUTABLE"], "binary")
         self.assertEqual(
             os.environ["FZFAWS_FZF_KEYS"],
             "--bind=alt-a:toggle-all,alt-j:jump,alt-0:top,alt-s:toggle-sort",
         )
+
         self.assertRegex(os.environ["FZFAWS_FZF_OPTS"], r"^--color=dark\s--color=.*")
 
+        # reset
         os.environ["FZFAWS_FZF_EXECUTABLE"] = ""
         os.environ["FZFAWS_FZF_OPTS"] = ""
         os.environ["FZFAWS_FZF_KEYS"] = ""
-        self.fileloader._set_fzf_env({"executable": "system"})
-        self.assertEqual(os.getenv("FZFAWS_FZF_KEYS", ""), "")
-        self.assertEqual(os.getenv("FZFAWS_FZF_EXECUTABLE"), "system")
-        self.assertEqual(os.getenv("FZFAWS_FZF_OPTS", ""), "")
 
-    def test_set_fzf_env(self):
-        os.environ["FZFAWS_FZF_EXECUTABLE"] = ""
-        os.environ["FZFAWS_FZF_OPTS"] = ""
-        os.environ["FZFAWS_FZF_KEYS"] = ""
         # empty test
         self.fileloader._set_fzf_env({})
         self.assertEqual(os.getenv("FZFAWS_FZF_KEYS", ""), "")
