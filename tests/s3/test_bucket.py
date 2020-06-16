f2885d4e08dfb916e2a72162c57fe661c09c7e3e
kevin zhuang <kevin7441@gmail.com>
2020-06-16 11:21:41 +1000

    test(s3): test sync and recursive copy

---
+++ tests/s3/test_bucket.py
@@ -0,0 +1,101 @@
+import io
+import sys
+import unittest
+from unittest.mock import call, patch
+from fzfaws.s3.bucket_s3 import bucket_s3
+from fzfaws.s3 import S3
+
+
+class TestS3BucketCopy(unittest.TestCase):
+    def setUp(self):
+        self.capturedOutput = io.StringIO()
+        sys.stdout = self.capturedOutput
+
+    def tearDown(self):
+        sys.stdout = sys.__stdout__
+
+    @patch.object(S3, "set_s3_bucket")
+    @patch.object(S3, "get_object_version")
+    @patch.object(S3, "set_s3_object")
+    @patch.object(S3, "set_s3_path")
+    @patch("fzfaws.s3.bucket_s3.sync_s3")
+    def test_sync(
+        self, mocked_sync, mocked_path, mocked_object, mocked_version, mocked_bucket
+    ):
+        bucket_s3(sync=True, exclude=["*"], include=["hello*"])
+        mocked_object.assert_not_called()
+        mocked_version.assert_not_called()
+        mocked_bucket.assert_has_calls(
+            [
+                call(
+                    header="Set the source bucket which contains the file to transfer"
+                ),
+                call(
+                    header="Set the destination bucket where the file should be transfered"
+                ),
+            ]
+        )
+        mocked_sync.assert_called_with(["*"], ["hello*"], "s3:///", "s3:///")
+
+        bucket_s3(
+            sync=True,
+            exclude=["*"],
+            include=["hello*"],
+            from_bucket="kazhala-lol/",
+            to_bucket="kazhala-yes/foo/",
+            version=True,
+        )
+        mocked_version.assert_not_called()
+        mocked_object.assert_not_called()
+        mocked_sync.assert_called_with(
+            ["*"], ["hello*"], "s3://kazhala-lol/", "s3://kazhala-yes/foo/"
+        )
+
+    @patch.object(S3, "set_s3_path")
+    @patch.object(S3, "set_s3_bucket")
+    @patch.object(S3, "get_object_version")
+    @patch("fzfaws.s3.bucket_s3.walk_s3_folder")
+    @patch("fzfaws.s3.bucket_s3.get_confirmation")
+    def test_recusive(
+        self, mocked_confirm, mocked_walk, mocked_version, mocked_bucket, mocked_path
+    ):
+        self.capturedOutput.truncate(0)
+        self.capturedOutput.seek(0)
+        mocked_confirm.return_value = False
+        mocked_walk.side_effect = lambda a, b, c, d, e, g, h, i, j, k: print(
+            b, c, d, e, g, h, i, j, k
+        )
+        bucket_s3(
+            recursive=True,
+            from_bucket="kazhala-lol/hello/",
+            to_bucket="kazhala-yes/foo/",
+            exclude=["*"],
+            include=["foo*"],
+            version=True,
+        )
+        self.assertEqual(
+            self.capturedOutput.getvalue(),
+            "kazhala-lol hello/ hello/ [] ['*'] ['foo*'] bucket foo/ kazhala-yes\n",
+        )
+        mocked_version.assert_not_called()
+
+        self.capturedOutput.truncate(0)
+        self.capturedOutput.seek(0)
+        bucket_s3(recursive=True)
+        self.assertEqual(
+            self.capturedOutput.getvalue(), "   [] [] [] bucket  \n",
+        )
+        mocked_bucket.assert_has_calls(
+            [
+                call(
+                    header="Set the source bucket which contains the file to transfer"
+                ),
+                call(
+                    header="Set the destination bucket where the file should be transfered"
+                ),
+            ]
+        )
+        mocked_path.assert_has_calls([call(), call()])
+
+    def test_version(self):
+        pass
