7273dd14434e9f77103be7b20b760f65fe1aa8d0
kevin zhuang <kevin7441@gmail.com>
2020-07-05 10:25:26 +1000

    test(cloudformation): test execute changeset

--- tests/cloudformation/test_changeset_stack.py
+++ tests/cloudformation/test_changeset_stack.py
@@ -10,6 +10,17 @@ class TestCloudformationChangesetStack(unittest.TestCase):
     def setUp(self):
         self.capturedOutput = io.StringIO()
         sys.stdout = self.capturedOutput
+        self.list_change_sets_value = {
+            "Summaries": [
+                {
+                    "ChangeSetName": "foo",
+                    "StackName": "testing1",
+                    "ExecutionStatus": "AVAILABLE",
+                    "Status": "CREATE_COMPLETE",
+                    "Description": "boo",
+                }
+            ]
+        }
 
     def tearDown(self):
         sys.stdout = sys.__stdout__
@@ -22,17 +33,9 @@ class TestCloudformationChangesetStack(unittest.TestCase):
         self.capturedOutput.seek(0)
         cloudformation = MockedCloudformation()
         cloudformation.stack_name = "testing1"
-        cloudformation.client.list_change_sets.return_value = {
-            "Summaries": [
-                {
-                    "ChangeSetName": "foo",
-                    "StackName": "testing1",
-                    "ExecutionStatus": "AVAILABLE",
-                    "Status": "CREATE_COMPLETE",
-                    "Description": "boo",
-                }
-            ]
-        }
+        cloudformation.client.list_change_sets.return_value = (
+            self.list_change_sets_value
+        )
         mocked_execute.return_value = "fooboo"
 
         cloudformation.client.describe_change_set.return_value = {"Changes": {}}
@@ -42,15 +45,7 @@ class TestCloudformationChangesetStack(unittest.TestCase):
             StackName="testing1"
         )
         mocked_process.assert_called_once_with(
-            [
-                {
-                    "ChangeSetName": "foo",
-                    "StackName": "testing1",
-                    "ExecutionStatus": "AVAILABLE",
-                    "Status": "CREATE_COMPLETE",
-                    "Description": "boo",
-                }
-            ],
+            self.list_change_sets_value["Summaries"],
             "ChangeSetName",
             "StackName",
             "ExecutionStatus",
@@ -63,3 +58,38 @@ class TestCloudformationChangesetStack(unittest.TestCase):
         )
         self.assertRegex(self.capturedOutput.getvalue(), r"StackName: testing1")
         self.assertRegex(self.capturedOutput.getvalue(), r"ChangeSetName: fooboo")
+
+    @patch("fzfaws.cloudformation.changeset_stack.get_confirmation")
+    @patch.object(Pyfzf, "process_list")
+    @patch.object(Pyfzf, "execute_fzf")
+    @patch("fzfaws.cloudformation.changeset_stack.Cloudformation")
+    def test_execute_changeset(
+        self, MockedCloudformation, mocked_execute, mocked_list, mocked_confirm
+    ):
+        mocked_confirm.return_value = True
+        cloudformation = MockedCloudformation()
+        cloudformation.stack_name = "testing1"
+        cloudformation.client.list_change_sets.return_value = (
+            self.list_change_sets_value
+        )
+        mocked_execute.return_value = "fooboo"
+        changeset_stack(execute=True, delete=True)
+        cloudformation.client.delete_change_set.assert_not_called()
+        cloudformation.set_stack.assert_called_once()
+        cloudformation.client.list_change_sets.assert_called_once_with(
+            StackName="testing1"
+        )
+        mocked_list.assert_called_once_with(
+            self.list_change_sets_value["Summaries"],
+            "ChangeSetName",
+            "StackName",
+            "ExecutionStatus",
+            "Status",
+            "Description",
+        )
+        cloudformation.client.execute_change_set.assert_called_once_with(
+            ChangeSetName="fooboo", StackName="testing1"
+        )
+        cloudformation.wait.assert_called_once_with(
+            "stack_update_complete", "Wating for stack to be updated ..."
+        )
