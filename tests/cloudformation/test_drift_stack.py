b725c44971f1c78ff0fca28c7c348fdf15f217e4
Kevin Zhuang <kevin7441@gmail.com>
2020-07-06 11:06:51 +1000

    test(cloudformation): test drift detection of entire stack

--- tests/cloudformation/test_drift_stack.py
+++ tests/cloudformation/test_drift_stack.py
@@ -10,16 +10,7 @@ class TestCloudformationDriftStack(unittest.TestCase):
     def setUp(self):
         self.capturedOutput = io.StringIO()
         sys.stdout = self.capturedOutput
-
-    def tearDown(self):
-        sys.stdout = sys.__stdout__
-
-    @patch("fzfaws.cloudformation.drift_stack.Cloudformation")
-    def test_info(self, MockedCloudformation):
-        self.capturedOutput.truncate(0)
-        self.capturedOutput.seek(0)
-        cloudformation = MockedCloudformation()
-        cloudformation.stack_details = {
+        self.cloudformation_details = {
             "StackId": "arn:aws:cloudformation:ap-southeast-2:1111111:stack/dotbare-cicd/0ae5ef60-9651-11ea-b6d0-0223bf2782f0",
             "StackName": "dotbare-cicd",
             "Description": "CodeBuild template for dotbare, webhook trigger from Github only on Master push",
@@ -31,6 +22,16 @@ class TestCloudformationDriftStack(unittest.TestCase):
             "Tags": [],
             "DriftInformation": {"StackDriftStatus": "IN_SYNC"},
         }
+
+    def tearDown(self):
+        sys.stdout = sys.__stdout__
+
+    @patch("fzfaws.cloudformation.drift_stack.Cloudformation")
+    def test_info(self, MockedCloudformation):
+        self.capturedOutput.truncate(0)
+        self.capturedOutput.seek(0)
+        cloudformation = MockedCloudformation()
+        cloudformation.stack_details = self.cloudformation_details
         cloudformation.stack_name = "testing1"
         cloudformation.client.describe_stack_resource_drifts.return_value = {}
         drift_stack(profile="root", region="us-east-1", info=True)
@@ -39,6 +40,29 @@ class TestCloudformationDriftStack(unittest.TestCase):
         cloudformation.client.describe_stack_resource_drifts.assert_called_once_with(
             StackName="testing1"
         )
+        self.assertRegex(
+            self.capturedOutput.getvalue(), r'"StackDriftStatus": "IN_SYNC"'
+        )
+
+    @patch("fzfaws.cloudformation.drift_stack.wait_drift_result")
+    @patch("fzfaws.cloudformation.drift_stack.Cloudformation")
+    def test_drift_entire_stack(self, MockedCloudformation, mocked_wait):
+        self.capturedOutput.truncate(0)
+        self.capturedOutput.seek(0)
+        cloudformation = MockedCloudformation()
+        cloudformation.stack_name = "testing1"
+        cloudformation.stack_details = self.cloudformation_details
+        cloudformation.client.detect_stack_drift.return_value = {
+            "StackDriftDetectionId": "1111111"
+        }
+        drift_stack(wait=True)
+        MockedCloudformation.assert_called_with(False, False)
+        mocked_wait.assert_called_once_with(cloudformation, "1111111")
+        cloudformation.client.detect_stack_drift.assert_called_once_with(
+            StackName="testing1"
+        )
+        self.assertRegex(self.capturedOutput.getvalue(), r"Drift detection initiated")
+        self.assertRegex(self.capturedOutput.getvalue(), r"DriftDetectionId: 1111111")
         self.assertRegex(
             self.capturedOutput.getvalue(), r'"StackDriftStatus": "IN_SYNC"'
         )
