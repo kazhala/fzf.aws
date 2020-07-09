1bd3792d630f5585cab4389ec4358648f2b6fc56
Kevin Zhuang <kevin7441@gmail.com>
2020-07-10 09:25:12 +1000

    refactor(cli): use argparse

--- fzfaws/cli.py
+++ fzfaws/cli.py
@@ -1,53 +1,51 @@
-"""Contains the main cli entry command
+"""Contains the fzfaws main cli entry command.
 
 Typical usage example:
     fzfaws <command> --options
 """
+
+import argparse
 import sys
+
 from botocore.exceptions import ClientError
-from fzfaws.utils.exceptions import (
-    NoCommandFound,
-    NoSelectionMade,
-    InvalidFileType,
-)
+
 from fzfaws.cloudformation.main import cloudformation
 from fzfaws.ec2.main import ec2
 from fzfaws.s3.main import s3
 from fzfaws.utils import FileLoader, get_default_args
+from fzfaws.utils.exceptions import InvalidFileType, NoSelectionMade
 
 
 def main() -> None:
-    """Entry function of the fzf.aws module
-    """
-
+    """Entry function of the fzf.aws module."""
     try:
+        parser = argparse.ArgumentParser(
+            description="A interactive aws cli experience with the help of fzf",
+            prog="fzfaws",
+        )
+        subparsers = parser.add_subparsers(dest="subparser_name")
+        subparsers.add_parser("cloudformation")
+        subparsers.add_parser("ec2")
+        subparsers.add_parser("s3")
+
         if len(sys.argv) < 2:
-            raise NoCommandFound()
-        available_routes = ["cloudformation", "ec2", "s3"]
-        action_command = sys.argv[1]
-        if action_command not in available_routes:
-            raise NoCommandFound()
+            parser.print_help()
+            raise SystemExit
+
+        args = parser.parse_args([sys.argv[1]])
+
         fileloader = FileLoader()
         fileloader.load_config_file()
 
-        argument_list = get_default_args(action_command, sys.argv[2:])
+        argument_list = get_default_args(args.subparser_name, sys.argv[2:])
 
-        if action_command == "cloudformation":
+        if args.subparser_name == "cloudformation":
             cloudformation(argument_list)
-        elif action_command == "ec2":
+        elif args.subparser_name == "ec2":
             ec2(argument_list)
-        elif action_command == "s3":
+        elif args.subparser_name == "s3":
             s3(argument_list)
 
-    # display help message
-    # did'n use argparse at the entry level thus creating similar help message
-    except NoCommandFound as e:
-        print("usage: fzfaws [-h] {cloudformation,ec2,s3} ...\n")
-        print("A interactive aws cli experience with the help of fzf\n")
-        print("positional arguments:")
-        print("  {cloudformation,ec2,s3}\n")
-        print("optional arguments:")
-        print("  -h, --help            show this help message and exit")
     except InvalidFileType:
         print("Selected file is not a valid template file type")
     except (KeyboardInterrupt, SystemExit, SystemError):
