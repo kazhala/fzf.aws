9c9b086c7c2cf1a8cf29577eb1b8c498fab30de6
Kevin Zhuang <kevin7441@gmail.com>
2020-07-23 11:12:40 +1000

    test(cli): improve coverage

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,5 @@
+from botocore.exceptions import ClientError
+from fzfaws.utils.exceptions import InvalidFileType
 import os
 from fzfaws.utils.fileloader import FileLoader
 import unittest
@@ -20,9 +22,10 @@ class TestCLI(unittest.TestCase):
     def tearDown(self):
         sys.stdout = sys.__stdout__
 
+    @patch("fzfaws.cli.s3")
     @patch("fzfaws.cli.ec2")
     @patch("fzfaws.cli.cloudformation")
-    def test_subparser(self, mocked_cloudformation, mocked_ec2):
+    def test_subparser(self, mocked_cloudformation, mocked_ec2, mocked_s3):
         sys.argv = [__file__, "cloudformation", "-h"]
         main()
         mocked_cloudformation.assert_called_once_with(["-h"])
@@ -36,6 +39,10 @@ class TestCLI(unittest.TestCase):
         main()
         mocked_ec2.assert_called_once_with(["start", "--wait"])
 
+        sys.argv = [__file__, "s3", "download"]
+        main()
+        mocked_s3.assert_called_once_with(["download", "--hidden"])
+
     @patch("fzfaws.cli.copy_config")
     def test_parser(self, mocked_copy):
         sys.argv = [__file__, "-h"]
@@ -48,9 +55,36 @@ class TestCLI(unittest.TestCase):
         self.assertRaises(SystemExit, main)
         mocked_copy.assert_called_once()
 
+        self.capturedOuput.truncate(0)
+        self.capturedOuput.seek(0)
+        sys.argv = [__file__]
+        self.assertRaises(SystemExit, main)
+        self.assertRegex(self.capturedOuput.getvalue(), r"^usage: fzfaws \[-h\].*")
+
     def test_copy_config(self):
         with tempfile.TemporaryDirectory() as tmpdirname:
             os.environ["XDG_CONFIG_HOME"] = tmpdirname
             copy_config()
             if not Path("%s/fzfaws/fzfaws.yml" % tmpdirname).is_file():
                 self.fail("config file not properly copied")
+
+    @patch("fzfaws.cli.get_default_args")
+    def test_exceptions(self, mocked_args):
+        mocked_args.side_effect = InvalidFileType
+        sys.argv = [__file__, "s3"]
+        main()
+        self.assertEqual(
+            self.capturedOuput.getvalue(), "Selected file is not a valid file type\n"
+        )
+
+        mocked_args.side_effect = SystemExit
+        sys.argv = [__file__, "s3"]
+        self.assertRaises(SystemExit, main)
+
+        mocked_args.side_effect = KeyboardInterrupt
+        sys.argv = [__file__, "s3"]
+        self.assertRaises(SystemExit, main)
+
+        mocked_args.side_effect = ClientError
+        sys.argv = [__file__, "s3"]
+        main()
