ce3e71a6cafa1e9840ab0bb4d67f6644f9591f25
Kevin Zhuang <kevin7441@gmail.com>
2020-07-31 09:48:12 +1000

    test(lambdaf): test main function

---
+++ tests/lambdaf/test_main.py
@@ -0,0 +1,32 @@
+import io
+import sys
+import unittest
+from unittest.mock import patch
+from fzfaws.lambdaf.main import lambdaf
+
+
+class TestLambdafMain(unittest.TestCase):
+    def setUp(self):
+        self.capturedOutput = io.StringIO()
+        sys.stdout = self.capturedOutput
+
+    def tearDown(self):
+        sys.stdout = sys.__stdout__
+
+    def test_help(self):
+        self.assertRaises(SystemExit, lambdaf, ["-h"])
+        self.assertRegex(self.capturedOutput.getvalue(), r"usage: fzfaws lambda \[-h\]")
+
+        self.capturedOutput.truncate(0)
+        self.capturedOutput.seek(0)
+        self.assertRaises(SystemExit, lambdaf, ["invoke", "-h"])
+        self.assertRegex(
+            self.capturedOutput.getvalue(), r"usage: fzfaws lambda invoke \[-h\]"
+        )
+
+        self.assertRaises(SystemExit, lambdaf, [])
+
+    @patch("fzfaws.lambdaf.main.invoke_function")
+    def test_invoke(self, mocked_lambdaf):
+        lambdaf(["invoke", "--all", "--async"])
+        mocked_lambdaf.assert_called_once_with(False, False, True, True)
