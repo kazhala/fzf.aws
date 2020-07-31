292571942ef4e051383eb9c4729e2dd331350628
Kevin Zhuang <kevin7441@gmail.com>
2020-07-31 10:27:39 +1000

    test(lambdaf): added test for async invoke

--- tests/lambdaf/test_invoke_function.py
+++ tests/lambdaf/test_invoke_function.py
@@ -6,7 +6,7 @@ from unittest.mock import patch
 from fzfaws.lambdaf.invoke_function import (
     invoke_function,
     invoke_function_sync,
-    get_function_name,
+    invoke_function_async,
 )
 
 
@@ -18,18 +18,29 @@ class TestLambdafInvoke(unittest.TestCase):
     def tearDown(self):
         sys.stdout = sys.__stdout__
 
+    @patch("fzfaws.lambdaf.invoke_function.invoke_function_async")
     @patch("fzfaws.lambdaf.invoke_function.invoke_function_sync")
     @patch("fzfaws.lambdaf.invoke_function.Lambdaf")
-    def test_invoke_function(self, MockedLambdaf, mocked_sync):
+    def test_invoke_function(self, MockedLambdaf, mocked_sync, mocked_async):
         lambdaf = MockedLambdaf.return_value
         invoke_function(all_version=True)
         lambdaf.set_lambdaf.assert_called_once_with(
             header="select function to invoke", all_version=True
         )
         mocked_sync.assert_called_once()
+        mocked_async.assert_not_called()
 
         invoke_function(all_version=True, profile=True, region="us-east-2")
         MockedLambdaf.assert_called_with(True, "us-east-2")
+
+        lambdaf.set_lambdaf.reset_mock()
+        mocked_sync.reset_mock()
+        invoke_function(all_version=True, asynk=True)
+        lambdaf.set_lambdaf.assert_called_once_with(
+            header="select function to invoke", all_version=True
+        )
+        mocked_sync.assert_not_called()
+        mocked_async.assert_called_once()
 
     @patch("fzfaws.lambdaf.invoke_function.base64")
     @patch("fzfaws.lambdaf.invoke_function.json")
@@ -59,4 +70,30 @@ class TestLambdafInvoke(unittest.TestCase):
         invoke_function_sync(lambdaf)
         lambdaf.client.invoke.assert_called_once_with(
             FunctionName="testing:1", InvocationType="RequestResponse", LogType="Tail"
+        )
+
+    @patch("fzfaws.lambdaf.invoke_function.Lambdaf")
+    def test_invoke_function_async(self, MockedLambdaf):
+        lambdaf = MockedLambdaf.return_value
+        lambdaf.function_detail = {
+            "FunctionName": "testing",
+            "Runtime": "python3.8",
+            "Version": "$LATEST",
+            "Description": None,
+        }
+        invoke_function_async(lambdaf)
+        lambdaf.client.invoke.assert_called_once_with(
+            FunctionName="testing", InvocationType="Event", LogType="Tail"
+        )
+
+        lambdaf.client.invoke.reset_mock()
+        lambdaf.function_detail = {
+            "FunctionName": "testing",
+            "Runtime": "python3.8",
+            "Version": "5",
+            "Description": None,
+        }
+        invoke_function_async(lambdaf)
+        lambdaf.client.invoke.assert_called_once_with(
+            FunctionName="testing:5", InvocationType="Event", LogType="Tail"
         )
